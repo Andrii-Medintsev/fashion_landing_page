7321913e7e223a7d1973b1bdad246ec1a80c3f94
Andrii Medintsev <medintsev.andrew@gmail.com>
2023-11-27 21:21:50 +0200

    Footer was implemented

--- src/js/app.js
+++ src/js/app.js
@@ -2,24 +2,5 @@
 import * as saFunctions from './modules/functions.js';
 saFunctions.isWebp();
 
-
-// ========  BURGER  ========================================================
-// import './modules/burger.js';
-
-
-// ========  PLAYER  ========================================================
-// import './modules/player.js';
-
-
-// ========  SWIPER  ========================================================
-// import Swiper, { Autoplay, Keyboard, Navigation } from 'swiper';
-// import './modules/mySwiper.js';
-
-// document.querySelector('.prev').onclick = () => {
-// 	swiper.slidePrev();
-// }
-// document.querySelector('.next').onclick = () => {
-// 	swiper.slideNext();
-// }
-
-// import "./modules/scrollAnimation.js"
+const form = document.querySelector('.footer__form');
+form.addEventListener('submit', (e) => e.preventDefault());
